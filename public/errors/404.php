10a3a2c1f4c0e111625c1b743391697e149cc2ba
SuperDed34 <maxnozord@gmail.com>
2022-11-27 13:40:50 +0300

    Add public\errors

--- public/errors/404.php
+++ public/errors/404.php
@@ -1,267 +1,67 @@
-<!doctype html>
-<html lang="en">
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
-<link rel="stylesheet" href="../css/main.css">
-<title>404 Страница не найдена</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Free 404 White for Iphone, Android & Smartphone Mobile Website Template | Home :: w3layouts</title>
+    <style type="text/css">
+        body{
+            font-family:Arial, Helvetica, sans-serif;
+        }
+        .wrap{
+            width:1000px;
+            margin:0 auto;
+        }
+        .logo{
+            width:430px;
+            position:absolute;
+            top:25%;
+            left:35%;
+        }
+        p a{
+            color:#eee;
+            font-size:13px;
+            margin-left:30px;
+            padding:5px;
+            background:#FF3366;
+            text-decoration:none;
+            -webkit-border-radius:.3em;
+            -moz-border-radius:.3em;
+            border-radius:.3em;
+        }
+        p a:hover{
+            color: #fff;
+        }
+        .footer{
+            position:absolute;
+            bottom:10px;
+            right:10px;
+            font-size:12px;
+            color:#aaa;
+        }
+        .footer a{
+            color:#666;
+            text-decoration:none;
+        }
+
+    </style>
 </head>
-
 <body>
-<header>
-    <div class="header-top py-3">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="col">
-                    <a href="tel:5551234567">
-                        <span class="icon-phone">&#9743;</span> 555 123-45-67
-                    </a>
-                </div>
-                <div class="col text-end icons">
-                    <form>
-                        <div class="input-group" id="search">
-                            <input type="text" class="form-control" placeholder="Search..." name="s">
-                            <button class="btn close-search" type="button"><i class="fas fa-times"></i></i></button>
-                            <button class="btn" type="submit"><i class="fas fa-search"></i></button>
-                        </div>
-                    </form>
-                    <a href="#" class="open-search"><i class="fas fa-search"></i></a>
-
-                    <a href="#" class="relative" data-bs-toggle="modal" data-bs-target="#cart-modal">
-                        <i class="fas fa-shopping-cart"></i>
-                        <span class="badge bg-danger rounded-pill count-items">0</span>
-                    </a>
-                    <div class="modal fade" id="cart-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Корзина</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <table class="table text-start">
-                                        <thead>
-                                        <tr>
-                                            <th scope="col">Фото</th>
-                                            <th scope="col">Товар</th>
-                                            <th scope="col">Кол-во</th>
-                                            <th scope="col">Цена</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <td>
-                                                <a href="#"><img src="img/products/apple_cinema_30.jpg" alt=""></a>
-                                            </td>
-                                            <td><a href="#">Apple cinema</a></td>
-                                            <td>1</td>
-                                            <td>100</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <a href="#"><img src="img/products/canon_eos_5d_1.jpg" alt=""></a>
-                                            </td>
-                                            <td><a href="#">Canon EOS</a></td>
-                                            <td>1</td>
-                                            <td>100</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <a href="#"><img src="img/products/hp_1.jpg" alt=""></a>
-                                            </td>
-                                            <td><a href="#">HP</a></td>
-                                            <td>1</td>
-                                            <td>100</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-danger ripple" data-bs-dismiss="modal">Продолжить покупки</button>
-                                    <button type="button" class="btn btn-primary">Оформить заказ</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <a href="#"><i class="far fa-heart"></i></a>
-
-                    <div class="dropdown d-inline-block">
-                        <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
-                            <i class="far fa-user"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Авторизация</a></li>
-                            <li><a class="dropdown-item" href="#">Регистрация</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="dropdown d-inline-block">
-                        <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
-                            <img src="img/ru.png" alt="">
-                        </a>
-                        <ul class="dropdown-menu" id="languages">
-                            <li>
-                                <button class="dropdown-item" data-langcode="en">
-                                    <img src="img/en.png" alt="">
-                                    English</button>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div><!-- header-top -->
-
-    <div class="header-bottom py-2">
-        <div class="container">
-
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid p-0">
-                    <a class="navbar-brand" href="index.html">Shop Brand</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="category.html">Компьютеры</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="category.html">Планшеты</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Ноутбуки
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="category.html">Mac</a></li>
-                                    <li><a class="dropdown-item" href="category.html">Windows</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="category.html">Телефоны</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="category.html">Камеры</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-            </nav>
-
-        </div>
-    </div><!-- header-bottom -->
-</header>
-
-<div class="special-404">
-    <div style="{
-   border-width: medium;
-        }">
-        }da</div>
-	<p><a href= "..\index.php">404</a></p><br>
-    <p><a href="">Страница не найдена</a></p>
+<div class="wrap">
+    <div class="logo">
+        <img src="/errors/images/404.png" alt=""  />
+        <p><a href="<?=PATH;?>">Go back to Home</a></p>
+    </div>
+</div>
+<div class="footer">
+    Design by - <a href="http://w3layouts.com">W3Layouts</a>
 </div>
 
-<section class="services">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h3 class="section-title">Наши преимущества</h3>
-            </div>
-
-            <div class="col-md-3 col-sm-6">
-                <div class="service-item">
-                    <p class="text-center"><i class="fas fa-shipping-fast"></i></p>
-                    <p>Прямые поставки от производителей</p>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6">
-                <div class="service-item">
-                    <p class="text-center"><i class="fas fa-cubes"></i></p>
-                    <p>Широкий ассортимент товара</p>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6">
-                <div class="service-item">
-                    <p class="text-center"><i class="fas fa-hand-holding-usd"></i></p>
-                    <p>Приятные и конкуретные цены</p>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6">
-                <div class="service-item">
-                    <p class="text-center"><i class="fas fa-user-cog"></i></p>
-                    <p>Профессиональная консультация и сервис</p>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-
-<footer>
-    <section class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-6">
-                    <h4>Информация</h4>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Главная</a></li>
-                        <li><a href="#">О магазине</a></li>
-                        <li><a href="#">Оплата и доставка</a></li>
-                        <li><a href="#">Контакты</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-3 col-6">
-                    <h4>Время работы</h4>
-                    <ul class="list-unstyled">
-                        <li>г. Киев, ул. Пушкина, 10</li>
-                        <li>пн-вс: 9:00 - 18:00</li>
-                        <li>без перерыва</li>
-                    </ul>
-                </div>
-
-                <div class="col-md-3 col-6">
-                    <h4>Контакты</h4>
-                    <ul class="list-unstyled">
-                        <li><a href="tel:5551234567">555 123-45-67</a></li>
-                        <li><a href="tel:5551234567">555 123-45-68</a></li>
-                        <li><a href="tel:5551234567">555 123-45-69</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-3 col-6">
-                    <h4>Мы в сети</h4>
-                    <div class="footer-icons">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-youtube"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</footer>
-
-<button id="top">
-    <i class="fas fa-angle-double-up"></i>
-</button>
-
-
-<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-        crossorigin="anonymous"></script>
-<script src="js/main.js"></script>
 </body>
+</html>
+
--- public/index.php
+++ public/index.php
@@ -5,7 +5,13 @@ if (PHP_MAJOR_VERSION < 8){
     die('Необходиа версия PHP >= 8');
 }
 new \max\App();
-throw new Exception('Произошло непредвиденное исключение');
-
+//throw new Exception('Произошло непредвиденное исключение');
 //echo \max\App::$app->getProperty('site_name');
 //var_dump(\max\App::$app->getProperties());
+
+?>
+
+<form method="post">
+
+    <button id="1" value="Ok">Нажми меня</button>
+</form>
